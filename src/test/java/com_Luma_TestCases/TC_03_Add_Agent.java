bfcc4e4a46474be54327797e5f448360a1f39e2e
Anwesh25 <anweshsriramoju7475@gmail.com>
2025-07-09 15:01:59 +0530

    This is my task related to AddAgent Functionality

---
+++ src/test/java/com_Luma_TestCases/TC_03_Add_Agent.java
@@ -0,0 +1,14 @@
+package com_Luma_TestCases;
+
+public class TC_03_Add_Agent {
+
+	public static void main(String[] args) {
+		
+		int a = 20;
+		int b = 30;
+		int c = 30;
+		System.out.println(a+b+c);
+
+	}
+
+}
